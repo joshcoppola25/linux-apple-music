8556399cab3db7df9d4dcdacf39ea4c9e51ac255
JCoppola <jcoppola@myseneca.ca>
2026-01-18 23:04:54 -0500

    put variable check after initialization

--- scripts/generateToken.js
+++ scripts/generateToken.js
@@ -1,17 +1,18 @@
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-if (!teamId || !keyId || !PRIVATE_KEY_PATH) {
-    console.error("No values found in .env");
-    process.exit(1);
-}
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const teamId = process.env.APPLE_TEAM_ID;
 const keyId = process.env.APPLE_KEY_ID;
 const PRIVATE_KEY_PATH = process.env.APPLE_PRIVATE_KEY_PATH;
 const privateKey = fs.readFileSync(PRIVATE_KEY_PATH, 'utf8');
+
+if (!teamId || !keyId || !PRIVATE_KEY_PATH) {
+    console.error("No values found in .env");
+    process.exit(1);
+}
 
 console.log("Team ID:", teamId);
 console.log("Key ID:", keyId);
